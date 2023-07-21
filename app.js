018e7f33b53010ff8c90dfa481a33d6fe3c29ccc
Ahmed Mansour <74689076+Ahmedmanso4r@users.noreply.github.com>
2023-07-21 16:52:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const result = require(`./result`)
+
+result.sum(3,4)
+result.substract(10,2)
+result.Multiplication(4,5)
+result.Division(10,5)
---
+++ result.js
@@ -0,0 +1,26 @@
+function sum(a,b){
+    console.log(a+b)
+
+}
+
+function substract(a,b){
+    console.log(a-b)
+
+}
+
+function Multiplication(a,b){
+    console.log(a*b)
+
+}
+
+function Division(a,b){
+    console.log(a/b)
+
+}
+
+module.exports = {
+    sum : sum ,
+    substract : substract ,
+    Multiplication : Multiplication ,
+    Division : Division
+}
